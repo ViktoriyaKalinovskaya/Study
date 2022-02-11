3ee20e0584dd39868a97ca7e11f327762027af29
viktoria <kalinovskaya000@mail.ru>
2022-02-11 13:52:14 +0300

    new kalkulator3 i meloch

---
+++ probnyi/17. ООП. Class/17.1.py
@@ -0,0 +1,4 @@
+def povtor(x):
+    d = int(x) + int(2 * x) + int(3 * x)
+    return d
+print(povtor(input('Введите число: ')))
---
+++ probnyi/17. ООП. Class/17.2.py
@@ -0,0 +1,31 @@
+# class Car: # название класса всегда с большой буквы
+#     color = 'Grey'
+#     def turn_on(self):
+#         pass
+#     def ride(self):
+#         pass
+# car_object = Car() # создание объекта класса
+# print(dir(Car))
+
+
+class Car:
+    # Статитеские поля (переменные класса)
+    default_color = 'Grey'
+    default_weight = 5000
+    def __init__(self, color, model):
+        # Динамические поля (переменные объекта)
+        self.color = color
+        self.model = model
+    def turn_on(self):
+        pass
+    def info(self):
+        print(self.color, self.model)
+car_obj_1 = Car('blue', 'BMW')
+car_obj_2 = Car('red', 'BMW2')
+car_obj_2.turn_on()
+print(car_obj_1.color)
+print(car_obj_1.model)
+car_obj_1.info()
+car_obj_2.info()
+# car_obj_1.default_color='red'
+# print(car_obj_1.default_color)
